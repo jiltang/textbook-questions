ee37eff81bc818fb2ff1e6b8a89caba2b392a6c7
Jillian Tang <jilingt@stanford.edu>
2019-11-13 02:02:37 -0800

    some form of matching topics

--- readJson.py
+++ readJson.py
@@ -1,37 +1,36 @@
 import json
-from wordfreq import zipf_frequency
+from wordfreq import top_n_list
+import string
 
 with open('tqa_train_val_test/val/tqa_v1_val.json', 'r') as f:
     data = json.load(f)
 
 print(len(data))
 
+raw_texts = []
+text_sets = []
+
+mostCommon = set(top_n_list('en', 500))
+
 for topic, mapping in data[1]['topics'].items():
     print(topic)
     print(mapping['content']['text'])
+    raw_texts.append(mapping['content']['text'])
+    text = mapping['content']['text'].translate(str.maketrans('', '', string.punctuation)).lower()
+    text = set(text.split()).difference(mostCommon)
+    text_sets.append(text)
 
 for questionID, mapping in data[1]['questions']['nonDiagramQuestions'].items():
-    if mapping['questionSubType'] == "True or False":
-        # print(questionID)
-        # print(mapping['globalID'])
-        # print(mapping['questionSubType'])
+    if mapping['questionSubType'] == "True or False" or mapping['questionSubType'] == "Matching":
         question = mapping['beingAsked']['processedText']
-        print(question)
-        freqs = [zipf_frequency(word, 'en') for word in question.split()]
-        min_index = min(enumerate(freqs), key=lambda x: x[1])[0]
-        for freq, word in zip(freqs, question.split()):
-            print(word, "\t\t", freq)
-        print("least common word:", question.split()[min_index].lower())
+        question = question.translate(str.maketrans('', '', string.punctuation)).lower()
+        words = set(question.split()).difference(mostCommon)
         correct = mapping['correctAnswer']['processedText']
-        print("Correct answer:", mapping['answerChoices'][correct]['processedText'])
-    # for answerChoice, answerMapping in mapping['answerChoices'].items():
-    #      print(answerChoice, answerMapping['processedText'])
-    #      answer = answerMapping['processedText']
-         #if answer == "True" or answer == "False":
-
-    # correct = mapping['correctAnswer']['rawText']
-    # print("Correct answer:", mapping['answerChoices'][correct])
-
-# for topic, mapping in data[0]['questions'].items():
-#     print(topic)
-#     print(mapping['content']['text'])
+        answer = mapping['answerChoices'][correct]['processedText']
+        words.update(answer.lower().split())
+        set_match = [len(words.intersection(text_set)) / len(text_set) for text_set in text_sets]
+        max_index = max(enumerate(set_match), key = lambda x: x[1])[0]
+        print("Question:", question)
+        print("Correct answer:", answer)
+        print("Matched with:\n", raw_texts[max_index])
+        print()
