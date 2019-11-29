ac6e372c3e0d7342e7284de42035818610ea2cdf
Jillian Tang <jilingt@stanford.edu>
2019-11-29 00:32:52 -0800

    added gpu functionality

--- run_opennmt.sh
+++ run_opennmt.sh
@@ -3,5 +3,9 @@
 # you'll have to install the glove embeddings into the specified directory
 # onmt_preprocess -train_src openNMTraw/src_train.txt -train_tgt openNMTraw/tgt_train.txt -valid_src openNMTraw/src_val.txt -valid_tgt openNMTraw/tgt_val.txt -save_data openNMTdata/data
 # OpenNMT-py-master/tools/embeddings_to_torch.py -emb_file_both glove_dir/glove.6B.100d.txt -dict_file openNMTdata/data.vocab.pt -output_file openNMTdata/embeddings
-onmt_train -data openNMTdata/data -save_model opennmt-model -word_vec_size 100 -pre_word_vecs_enc openNMTdata/embeddings.enc.pt -pre_word_vecs_dec openNMTdata/embeddings.dec.pt
+if [ "$1" == "-gpu" ]; then
+	onmt_train -data openNMTdata/data -save_model opennmt-model -word_vec_size 100 -pre_word_vecs_enc openNMTdata/embeddings.enc.pt -pre_word_vecs_dec openNMTdata/embeddings.dec.pt $2
+else
+	onmt_train -data openNMTdata/data -save_model opennmt-model -word_vec_size 100 -pre_word_vecs_enc openNMTdata/embeddings.enc.pt -pre_word_vecs_dec openNMTdata/embeddings.dec.pt
+fi
 onmt_translate -model opennmt-model_acc_XX.XX_ppl_XXX.XX_eX.pt -src openNMTraw/src_test.txt -output openNMTdata/pred.txt -replace_unk -verbose
